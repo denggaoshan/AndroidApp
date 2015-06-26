e9c68fda0b40e0e898a0d170275014ea049caef5
S <821465068@qq.com>
2015-06-27 01:34:04 +0800

    页面连接
    此处需要什么说明?

--- AndroidApp/app/src/main/java/com/example/administrator/androidapp/MainActivity.java
+++ AndroidApp/app/src/main/java/com/example/administrator/androidapp/MainActivity.java
@@ -4,6 +4,12 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.EditText;
+
+import org.apache.http.protocol.HTTP;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -11,7 +17,23 @@ public class MainActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.registered);
+        setContentView(R.layout.activity_main);
+        Button loadBtn = (Button)findViewById(R.id.loadButton);
+        loadBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setContentView(R.layout.registered);
+                EditText account_RegTxt = (EditText)findViewById(R.id.account_RegText);
+                //account_RegTxt.setText("sss");
+                //WebView webView = (WebView)findViewById(R.id.webView);
+                //webView.loadUrl("https://www.baidu.com/s?ie=utf-8&f=8&rsv_bp=1&rsv_idx=1&tn=baidu&wd=URLconnection%20connect&rsv_pq=dc17b74400014a8f&rsv_t=1f01R9YxbSGroJtpnP%2FUaxLenHl4KoeieexSxaxkf3WZblM8Jyw%2Bt9opbqI&rsv_enter=0&rsv_jmp=slow");
+                String s = HttpRequest.sendGet("http://www.tuling123.com/openapi/api", "key=3235efe35b29467f2dffebd09b510a20&info=ÄãºÃ");
+                System.out.println(s);
+                account_RegTxt.setText("asdad" + s)
+
+            }
+        });
+
     }
 
     @Override

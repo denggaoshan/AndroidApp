b24370b5c256ffe8e76a5c47199668dfdba53395
denggaoshan <475665610@qq.com>
2015-06-26 22:18:18 +0800

    完成注册页面
    贤哥求绑定：
    1. 登陆页面的注册按钮点了之后跳转到注册页面
    2. 注册页面中点击X，退回之前

--- AndroidApp/app/src/main/java/com/example/administrator/androidapp/MainActivity.java
+++ AndroidApp/app/src/main/java/com/example/administrator/androidapp/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.registered);
     }
 
     @Override

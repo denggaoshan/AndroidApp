4a0546eca41ecca0757f4334c4c72b1f831e30eb
denggaoshan <475665610@qq.com>
2015-06-28 18:13:02 +0800

    total_activity
    做了导航页面

--- AndroidApp/app/src/main/java/com/example/administrator/androidapp/MainActivity.java
+++ AndroidApp/app/src/main/java/com/example/administrator/androidapp/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.registered);
+        setContentView(R.layout.total_activities);
     }
 
     @Override

109c588e046e7c41fbda305d604b3bba791fae7d
denggaoshan <475665610@qq.com>
2015-06-29 15:37:20 +0800

    绑定界面
    绑定几个按钮

--- AndroidApp/app/src/main/java/com/example/administrator/androidapp/MainActivity.java
+++ AndroidApp/app/src/main/java/com/example/administrator/androidapp/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.administrator.androidapp;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.os.StrictMode;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
@@ -23,65 +25,26 @@ public class MainActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
+        setContentView(R.layout.activity_main);
         StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder().detectDiskReads().detectDiskWrites().detectNetwork().penaltyLog().build());
         StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder().detectLeakedSqlLiteObjects().detectLeakedClosableObjects().penaltyLog().penaltyDeath().build());
 
-        Button loadBtn = (Button)findViewById(R.id.loadButton);
-        loadBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                setContentView(R.layout.registered);
-                EditText account_RegTxt = (EditText)findViewById(R.id.account_RegText);
-                //account_RegTxt.setText("sss");
-                //WebView webView = (WebView)findViewById(R.id.webView);
-                //webView.loadUrl("https://www.baidu.com/s?ie=utf-8&f=8&rsv_bp=1&rsv_idx=1&tn=baidu&wd=URLconnection%20connect&rsv_pq=dc17b74400014a8f&rsv_t=1f01R9YxbSGroJtpnP%2FUaxLenHl4KoeieexSxaxkf3WZblM8Jyw%2Bt9opbqI&rsv_enter=0&rsv_jmp=slow");
-/*                String s = HttpRequest.sendGet("http://www.tuling123.com/openapi/api", "key=3235efe35b29467f2dffebd09b510a20&info=ÄãºÃ");
-                System.out.println(s);
-                account_RegTxt.setText("asdad" + s);*/
-                try
-                {
-                    String urlNameString = "http://www.tuling123.com/openapi/api?key=3235efe35b29467f2dffebd09b510a20&info=ÄãºÃ";
-                    URL realUrl = new URL(urlNameString);
-                    URLConnection connection = realUrl.openConnection();
-                    connection.connect();
-                    BufferedReader in = new BufferedReader(new InputStreamReader(
-                            connection.getInputStream()));
-                    String result = "";
-                    String line;
-                    try
-                    {
-                        while ((line = in.readLine()) != null) {
-                            result += line;
-                        }
-                    }
-                    catch(Exception ee)
-                    {
-                        EditText tell_RegText = (EditText)findViewById(R.id.tell_RegTxt);
-                        tell_RegText.setText("string exception");
-                    }
-                    finally {
-                        EditText account_RegText = (EditText)findViewById(R.id.account_RegText);
-                        account_RegText.setText(result);
-                    }
+    }
 
-                }
-                catch (Exception e)
-                {
-                    EditText tell_RegText = (EditText)findViewById(R.id.tell_RegTxt);
-                    tell_RegText.setText("exception" + "00" + e.getMessage() + "00" + e.getClass());
-                }
-            }
-        });
+    public void landing_Click(View v) {
+        Intent intent = new Intent();
+        intent.setClass(MainActivity.this, TotalActivity.class);
+        MainActivity.this.startActivity(intent);
+        MainActivity.this.finish();
+    }
 
-        Button registerBtn = (Button)findViewById(R.id.registerButton);
-/*        registerBtn.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
 
-            }
-        });*/
+    public void registered_Click(View v) {
+                Intent intent = new Intent();
+                intent.setClass(MainActivity.this, RegisteredActivity.class);
+                MainActivity.this.startActivity(intent);
+                MainActivity.this.finish();
     }
 
     @Override

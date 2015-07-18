125e886a2550f1418c3f9b8780baed62ea6ecf55
denggaoshan <475665610@qq.com>
2015-07-18 16:51:04 +0800

    小改

--- AndroidApp/app/src/main/java/com/example/administrator/androidapp/msg/Cache.java
+++ AndroidApp/app/src/main/java/com/example/administrator/androidapp/msg/Cache.java
@@ -6,10 +6,13 @@ import java.util.HashMap;
 
 /**
  * Created by admin on 2015/7/15.
- */
-public class Cache {
+ * 为了提高速度设置的缓存类
+ * 在程序空闲的时候，维护一个优先队列，通过空余的线程不断从服务器下载更新有可能用到的各种资料
+ * */
 
+public class Cache {
     private static Bitmap userAvater;
+
     public static void setUserAvater(Bitmap bm){
         userAvater = bm;
     }
@@ -18,11 +21,14 @@ public class Cache {
         return userAvater;
     }
 
-    private static HashMap<String, Bitmap> bitmapHashMap = new HashMap<>();
 
+    //存储图片
+    private static HashMap<String, Bitmap> bitmapCache = new HashMap<>();
+
+    //获得图片
     public static Bitmap getBitmap(String url){
-        if (bitmapHashMap.containsKey(url)){
-            return bitmapHashMap.get(url);
+        if (bitmapCache.containsKey(url)){
+            return bitmapCache.get(url);
         }
         else{
             Bitmap bitmap =  ToolClass.returnBitMap(url);
@@ -31,10 +37,29 @@ public class Cache {
             }
             return bitmap;
         }
-
     }
-    public static void setBitmap(String url, Bitmap bm){
-        bitmapHashMap.put(url, bm);
+
+    private static void setBitmap(String url, Bitmap bm){
+        bitmapCache.put(url, bm);
+    }
+
+
+    //存储用户信息
+    private static HashMap<String,User> userCache = new HashMap<>();
+
+
+    //根据ID获得用户
+    public static User getUserById(String userId){
+        if(userCache.containsKey(userId)){
+            return userCache.get(userId);
+        }else{
+            ToolClass.getUserInfo(User.getCurrentUser().getUserID(),userId);
+        }
+        return null;
+    }
+
+    public static void beginDownLoadInformation(){
+
     }
 
 }

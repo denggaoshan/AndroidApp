d147a8104f1df9d436131b09ee6a497f60fb2e2f
S <821465068@qq.com>
2015-07-21 17:23:35 +0800

    baidu地图定位及选定地址

--- AndroidApp/app/src/main/java/com/example/administrator/androidapp/page/Page_Map.java
+++ AndroidApp/app/src/main/java/com/example/administrator/androidapp/page/Page_Map.java
@@ -1,20 +1,37 @@
 package com.example.administrator.androidapp.page;
 
+
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
+import com.baidu.lbsapi.BMapManager;
+import com.baidu.location.BDLocation;
+import com.baidu.location.BDLocationListener;
+import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
 import com.baidu.mapapi.SDKInitializer;
 import com.baidu.mapapi.map.BaiduMap;
 import com.baidu.mapapi.map.BaiduMap.*;
 import com.baidu.mapapi.map.BitmapDescriptor;
 import com.baidu.mapapi.map.BitmapDescriptorFactory;
 import com.baidu.mapapi.map.MapPoi;
+import com.baidu.mapapi.map.MapStatus;
+import com.baidu.mapapi.map.MapStatusUpdate;
+import com.baidu.mapapi.map.MapStatusUpdateFactory;
 import com.baidu.mapapi.map.MapView;
 import com.baidu.mapapi.map.MarkerOptions;
 import com.baidu.mapapi.map.OverlayOptions;
 import com.baidu.mapapi.model.LatLng;
+import com.baidu.mapapi.search.core.SearchResult;
+import com.baidu.mapapi.search.geocode.GeoCodeResult;
+import com.baidu.mapapi.search.geocode.GeoCoder;
+import com.baidu.mapapi.search.geocode.OnGetGeoCoderResultListener;
+import com.baidu.mapapi.search.geocode.ReverseGeoCodeOption;
+import com.baidu.mapapi.search.geocode.ReverseGeoCodeResult;
+import com.baidu.nplatform.comapi.map.MapController;
 import com.example.administrator.androidapp.R;
 
 /**
@@ -24,6 +41,15 @@ public class Page_Map extends ActionBarActivity {
 
     MapView mMapView = null;
     BaiduMap mBaiduMap = null;
+    LocationClient mLocClient = null;
+    MapController mapController = null;
+    String resultAdress = "";
+    String resultLatitude = "";
+    String resultLontitude = "";
+    BMapManager mBMapManager = null;
+    GeoCoder geocoder = null;
+    MapStatus mapStatus = null;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,12 +58,112 @@ public class Page_Map extends ActionBarActivity {
         //注意该方法要再setContentView方法之前实现
         SDKInitializer.initialize(getApplicationContext());
         setContentView(R.layout.activity_map);
+        mBMapManager = new BMapManager(getApplication());
         //获取地图控件引用
         mMapView = (MapView) findViewById(R.id.bmapView);
         mBaiduMap = mMapView.getMap();
         mBaiduMap.setMapType(BaiduMap.MAP_TYPE_NORMAL);
         mBaiduMap.setOnMapClickListener(clickListener);
+        mBaiduMap.setMyLocationEnabled(true);
+        mLocClient = new LocationClient(this);
+        mLocClient.registerLocationListener(bdLocationListener);
+        LocationClientOption option = new LocationClientOption();
+        option.setOpenGps(true);// 打开gps
+        option.setCoorType("bd09ll"); // 设置坐标类型
+        option.setIsNeedAddress(true);
+        mLocClient.setLocOption(option);
+        mLocClient.start();
+        geocoder = GeoCoder.newInstance();
+        geocoder.setOnGetGeoCodeResultListener(geoListener);
+/*        mapStatus = new MapStatus.Builder()
+                .target(new LatLng(118.122691, 24.443522))
+                .build();
+        MapStatusUpdate mMapStatusUpdate = MapStatusUpdateFactory.newMapStatus(mapStatus);
+        mBaiduMap.setMapStatus(mMapStatusUpdate);*/
     }
+
+    BDLocationListener bdLocationListener = new BDLocationListener() {
+        @Override
+        public void onReceiveLocation(BDLocation bdLocation) {
+            if (bdLocation == null)
+                return;
+            StringBuffer sb = new StringBuffer(256);
+            sb.append("time : ");
+            sb.append(bdLocation.getTime());
+            sb.append("\nerror code : ");
+            sb.append(bdLocation.getLocType());
+            sb.append("\nlatitude : ");
+            sb.append(bdLocation.getLatitude());
+            sb.append("\nlontitude : ");
+            sb.append(bdLocation.getLongitude());
+            sb.append("\nradius : ");
+            sb.append(bdLocation.getRadius());
+            if (bdLocation.getLocType() == BDLocation.TypeGpsLocation) {// GPS定位结果
+                sb.append("\nspeed : ");
+                sb.append(bdLocation.getSpeed());// 单位：公里每小时
+                sb.append("\nsatellite : ");
+                sb.append(bdLocation.getSatelliteNumber());
+                sb.append("\nheight : ");
+                sb.append(bdLocation.getAltitude());// 单位：米
+                sb.append("\ndirection : ");
+                sb.append(bdLocation.getDirection());// 单位度
+                sb.append("\naddr : ");
+                sb.append(bdLocation.getAddrStr());
+                sb.append("\ndescribe : ");
+                sb.append("gps定位成功");
+            } else if (bdLocation.getLocType() == BDLocation.TypeNetWorkLocation) {// 网络定位结果
+                sb.append("\naddr : ");
+                sb.append(bdLocation.getAddrStr());
+                //运营商信息
+                sb.append("\noperationers : ");
+                sb.append(bdLocation.getOperators());
+                sb.append("\ndescribe : ");
+                sb.append("网络定位成功");
+            } else if (bdLocation.getLocType() == BDLocation.TypeOffLineLocation) {// 离线定位结果
+                sb.append("\ndescribe : ");
+                sb.append("离线定位成功，离线定位结果也是有效的");
+            } else if (bdLocation.getLocType() == BDLocation.TypeServerError) {
+                sb.append("\ndescribe : ");
+                sb.append("服务端网络定位失败，可以反馈IMEI号和大体定位时间到loc-bugs@baidu.com，会有人追查原因");
+            } else if (bdLocation.getLocType() == BDLocation.TypeNetWorkException) {
+                sb.append("\ndescribe : ");
+                sb.append("网络不同导致定位失败，请检查网络是否通畅");
+            } else if (bdLocation.getLocType() == BDLocation.TypeCriteriaException) {
+                sb.append("\ndescribe : ");
+                sb.append("无法获取有效定位依据导致定位失败，一般是由于手机的原因，处于飞行模式下一般会造成这种结果，可以试着重启手机");
+            }
+            mapStatus = new MapStatus.Builder()
+                    .target(new LatLng(bdLocation.getLatitude(), bdLocation.getLongitude()))
+                    .zoom(17)
+                    .build();
+            MapStatusUpdate mMapStatusUpdate = MapStatusUpdateFactory.newMapStatus(mapStatus);
+            mBaiduMap.setMapStatus(mMapStatusUpdate);
+        }
+    };
+
+    OnGetGeoCoderResultListener geoListener = new OnGetGeoCoderResultListener() {
+        // 反地理编码查询结果回调函数
+        @Override
+        public void onGetReverseGeoCodeResult(ReverseGeoCodeResult result) {
+            if (result == null
+                    || result.error != SearchResult.ERRORNO.NO_ERROR) {
+                // 没有检测到结果
+            }
+            Toast.makeText(Page_Map.this,
+                    "位置：" + result.getAddress(), Toast.LENGTH_LONG)
+                    .show();
+            resultAdress = result.getAddress();
+        }
+
+        // 地理编码查询结果回调函数
+        @Override
+        public void onGetGeoCodeResult(GeoCodeResult result) {
+            if (result == null
+                    || result.error != SearchResult.ERRORNO.NO_ERROR) {
+                // 没有检测到结果
+            }
+        }
+    };
 
     OnMapClickListener clickListener = new OnMapClickListener() {
         /**
@@ -47,12 +173,22 @@ public class Page_Map extends ActionBarActivity {
         public void onMapClick(LatLng point){
             BitmapDescriptor bitmap = BitmapDescriptorFactory
                     .fromResource(R.drawable.icon_close);
+            mBaiduMap.clear();
             OverlayOptions option = new MarkerOptions()
                     .position(point)
                     .icon(bitmap)
                     .zIndex(9)
                     .draggable(true);
             mBaiduMap.addOverlay(option);
+            //mLocClient.requestLocation();
+            geocoder.reverseGeoCode(new ReverseGeoCodeOption().location(point));
+            mapStatus = new MapStatus.Builder()
+                    .target(point)
+                    .build();
+            MapStatusUpdate mMapStatusUpdate = MapStatusUpdateFactory.newMapStatus(mapStatus);
+            mBaiduMap.setMapStatus(mMapStatusUpdate);
+            resultLatitude = "" + point.latitude;
+            resultLontitude = "" + point.longitude;
         }
         /**
          * 地图内 Poi 单击事件回调函数
